2d6699c56fa4c87fcdee879ddde9492a10518c4b
Alex Sherman <asherman.ca@gmail.com>
2023-06-28 17:07:39 -0700

    intercepting route modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
 	children,
+	authModal,
 }: {
 	children: React.ReactNode
+	authModal: React.ReactNode
 }) {
 	return (
 		<html
@@ -25,9 +27,10 @@ export default function RootLayout({
 			)}
 		>
 			<body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-				{/* @ts-ignore */}
+				{/* @ts-expect-error server component */}
 				<Navbar />
 				<div className='container max-w-7xl mx-auto h-full pt-12'>
+					{authModal}
 					{children}
 				</div>
 				<Toaster />
