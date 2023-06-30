4bb74830ce07aa7f575fa7ed9e98ee568a2881b4
Alex Sherman <asherman.ca@gmail.com>
2023-06-29 17:08:10 -0700

    schema update

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -5,7 +5,7 @@ import UserAuthForm from './UserAuthForm'
 
 const SignIn: FC = () => {
 	return (
-		<div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
+		<div className='container mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[400px]'>
 			<div className='flex flex-col space-y-2 text-center'>
 				<Icons.logo className='mx-auto h-6 w-6' />
 				<h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
